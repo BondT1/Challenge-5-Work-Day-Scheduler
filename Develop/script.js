5b93ef5bb52de2f7eb0ae5a348ad021d5df720c2
Tom Bond <tombond248@gmail.com>
2022-10-25 23:38:14 +0100

    progress on timeblock colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 var currentDateEl = $('#currentDay');
+var currentTime;
+var currentDate;
 
+var blockColors;
+var blockTime;
+
+// Current date and time shown on Jumbotron
 
 function dateDisplay() {
   var currentDate = moment().format('DD MMM YYYY [at] hh:mm:ss a');
@@ -7,3 +13,29 @@ function dateDisplay() {
 }
 
 setInterval(dateDisplay, 1000);
+
+
+
+
+function blockColors() {
+
+  var currentTime = moment().format('hh:mm:ss a');
+
+  $(".time-block").each(function () {
+      blockTime = parseInt($(this).attr("id").split('-')[1]);
+
+        if (blockTime < currentTime) {
+          $(this).removeClass('future present');
+          $(this).addClass('past');
+        } else if (blockTime == currentTime) {
+          $(this).removeClass('future past');
+          $(this).addClass('present');
+        } else {
+          $(this).removeClass('present past');
+          $(this).addClass('future');
+        }
+  })
+}
+
+blockColors();
+
